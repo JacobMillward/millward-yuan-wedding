859b6a4228442995400b7192a43852de4156290d
Jacob Millward <jacob@millward.dev>
2024-01-12 18:59:32 +0000

    Style the top section a little better

--- src/app/(sections)/top-section.tsx
+++ src/app/(sections)/top-section.tsx
@@ -5,13 +5,14 @@ import { NextSectionLink } from "./next-section-link";
 export const TopSection = () => {
   return (
     <PageSection id="top" image="/images/1.jpg">
-      <div className="flex flex-col m-auto items-center bg-slate-900/50 rounded-md p-10 text-white">
-        <h1 className="text-4xl font-bold">Jacob &amp; Jianan</h1>
-        <h2 className="text-2xl font-bold">are getting married!</h2>
+      <div className="p-10 m-auto">
+        <div className="flex flex-col m-auto items-center bg-slate-900/50 rounded-tl-3xl rounded-br-3xl p-10 text-white text-center">
+          <h1 className="text-4xl font-bold">Jacob &amp; Jianan</h1>
+          <h2 className="text-2xl font-bold">are getting married!</h2>
 
-        <p className="mt-3">
-          Saturday, November 16th, 2024 at Pepper Arden Hall
-        </p>
+          <p className="mt-3">Saturday, November 16th, 2024 · 12:30pm</p>
+          <p>Pepper Arden Hall, Northallerton</p>
+        </div>
       </div>
 
       <NextSectionLink target="agenda">What&apos;s the plan?</NextSectionLink>

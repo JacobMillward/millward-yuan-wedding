7f1c6e10b5309faf3c10d34f2b8242fabf5dc11f
Jacob Millward <jacob@millward.dev>
2024-02-17 14:09:30 +0000

    Update next-section-link colours to gold

--- src/app/(sections)/next-section-link.tsx
+++ src/app/(sections)/next-section-link.tsx
@@ -17,13 +17,13 @@ export const NextSectionLink: React.FC<Props> = ({
       className={`bg-slate-900/70 py-4 pl-4 rounded-l-3xl sticky bottom-16 my-16 ml-auto -mr-10 ${className}`}
     >
       <Link className="flex mr-4 items-center group" href={`#${target}`}>
-        <div className="relative text-white font-bold flex flex-col mt-0.5">
+        <div className="relative text-white font-bold flex flex-col mt-0.5 group-hover:text-gold transition-colors ease-out duration-300">
           {children}
-          <span className="mx-auto h-0.5 bg-white w-0 group-hover:w-full transition-all ease-out duration-300" />
+          <span className="mx-auto h-0.5 bg-white w-0 group-hover:w-full group-hover:bg-gold transition-all ease-out duration-300" />
         </div>
         <div className="ml-4 transition-all bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center mt-2 motion-safe:animate-bounce">
           <svg
-            className="w-6 h-6 text-slate-900 dark:text-slate-200 group-hover:text-blue-500 transition-all ease-out duration-300"
+            className="w-6 h-6 text-slate-900 dark:text-slate-200 group-hover:text-gold transition-all ease-out duration-300"
             fill="none"
             strokeLinecap="round"
             strokeLinejoin="round"

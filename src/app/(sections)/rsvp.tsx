c995b649f94f161aa6f63d0264d54052f57ebbdf
Jacob Millward <jacob@millward.dev>
2024-03-01 16:52:02 +0000

    Link to RSVP form

--- src/app/(sections)/rsvp.tsx
+++ src/app/(sections)/rsvp.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { PageSection, SectionBackground } from "./page-section";
+import NextLink from "next/link";
 import { Link } from "../link";
-
 export const RsvpSection = () => {
   return (
     <PageSection
@@ -12,15 +12,17 @@ export const RsvpSection = () => {
       <h1 className="text-4xl font-bold my-10">RSVP</h1>
       <p className="max-w-[900px] text-lg lg:text-xl">
         One you have received your invitation, please RSVP as soon as possible.
-        The quickest way to do this is to hit the button below and fill in the
-        form.
+        We will stop taking RSVPs after 16th June 2024. The quickest way to
+        respond is to hit the button below and fill in the form.
       </p>
-      <button
-        className="m-10 text-lg lg:text-xl bg-slate-900/50 rounded-xl p-5 text-white font-extrabold"
-        disabled
+      <NextLink
+        href="https://forms.gle/rjQtB6FFyAM86odu8"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="m-10 text-lg lg:text-xl bg-burgundy rounded-xl p-5 text-white font-extrabold hover:bg-burgundy/80 hover:text-gold transition-colors duration-300"
       >
-        RSVP is not yet open
-      </button>
+        Click here to RSVP
+      </NextLink>
       <p className="max-w-[900px] text-lg lg:text-xl mb-5">
         If you have any questions about the wedding, please contact us at{" "}
         <Link
